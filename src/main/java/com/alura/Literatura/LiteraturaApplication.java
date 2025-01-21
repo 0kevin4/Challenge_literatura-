c1711df9805164406d8a29d4374ad721693ddda4
0kevin4 <morenoramirezkevinjose@gmail.com>
2025-01-21 11:41:13 -0500

    Project spring initializr

--- src/main/java/com/alura/Literatura/LiteraturaApplication.java
+++ src/main/java/com/alura/Literatura/LiteraturaApplication.java
@@ -1,13 +1,28 @@
 package com.alura.Literatura;
 
+import com.alura.Literatura.repository.AuthorRepository;
+import com.alura.Literatura.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraturaApplication {
+public class LiteraturaApplication implements CommandLineRunner {
+
+	@Autowired
+	private BookRepository bookRepository;
+	@Autowired
+	private AuthorRepository authorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraturaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		var main = new Main(bookRepository, authorRepository);
+		main.showMenu();
 	}
 
 }
